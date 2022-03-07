21219e737d3b5dbf5616c27e01f3c9d292f8d031
senthamizhmaran r <Zenthamizhmaran10@gmial.com>
2022-03-07 09:54:13 +0530

    git edit

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -4,6 +4,7 @@ public class AxisBank  {
 	public void deposit () {
 		// TODO Auto-generated method stub
 		System.out.println("it is a fixed doposite");
+		System.out.println("git edited");
 
 	}
 
